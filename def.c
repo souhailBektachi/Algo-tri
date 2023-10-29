e601d2931b732490e1855b0450e5c9b4bbffc589
AbderrahimeEl <145778347+AbderrahimeEl@users.noreply.github.com>
2023-10-29 11:26:21 +0100

    calcule de temps

--- def.c
+++ def.c
@@ -35,3 +35,79 @@ void random_v(int *tab,int taille)
         tab[i]=rand() %100;
     }
 }
+// ----------------------------------------------------------------------------------------
+void trie_a_bul(int *tab,int taille)
+{
+   int i,j,temp,m;
+   do{
+    m=0;
+   for(i=0;i<taille-1;i++)
+   {
+    if(tab[i+1]<tab[i])
+    {
+      temp=tab[i];
+      tab[i]=tab[i+1];
+      tab[i+1]=temp;
+      m=1;
+    }
+   }
+   }while(m!=0);
+}
+// ----------------------------------------------------------------------------------------
+
+// Fonction qui mesure le temps d'exécution d'une autre fonction
+double mesureTemps(void (*fonction)(),int taille,int *tab) 
+{
+    // int *tab;
+    // tab=(int*)malloc(taille*sizeof(int));
+
+    clock_t start, end;
+    double cpu_time_used;
+
+    // Enregistrez le moment où vous démarrez la mesure du temps
+    start = clock();
+
+    // Appelez la fonction dont vous souhaitez mesurer le temps
+    fonction(tab,taille);
+    // Enregistrez le moment où la fonction a terminé
+    end = clock();
+    // Calculez le temps d'exécution en secondes
+    cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+    return cpu_time_used;
+}
+// ----------------------------------------------------------------------------------------
+void func()
+{
+    int i,j=0;
+    for(i=0;i<1000000;i++){j*=j;}
+}
+// ----------------------------------------------------------------------------------------
+void remplir_matrice_temp(double M[2][10])
+{
+    int i,j,taille=1000,*tab;
+    double t;
+    tab=(int*)malloc(taille*sizeof(int));
+    random_v(tab,taille);
+    for(i=0;i<10;i++)
+    {
+            M[0][i]=mesureTemps(trie_a_bul,taille,tab);
+            M[1][i]=mesureTemps(trie_par_selection,taille,tab);
+            taille+=3000;
+            tab=(int*)realloc(tab, taille * sizeof(int));
+            random_v(tab,taille);
+    }
+}
+// ----------------------------------------------------------------------------------------
+void affiche_matrice(double M[2][10])
+{
+    int i,j,taille=1000;
+    for(i=0;i<2;i++)
+    {
+        printf("\n");
+        for(j=0;j<10;j++)
+        {
+            printf("   %lf",M[i][j]);
+        }
+    }
+
+}
--- main.c
+++ main.c
@@ -1,12 +1,16 @@
 #include"algo_trie.h"
 int main(void)
 {
-    int taille=17,tab[17];
-    printf("\n test ... ");
-    random_v(tab,taille);
-    printf("\n test ... ");
-    printf("\n %d",tab[0]);
-    affiche_tab(tab,taille);
-    trie_par_selection(tab,taille);
-    affiche_tab(tab,taille);
+    // int taille=17,tab[17];
+    // printf("\n test ... ");
+    // random_v(tab,taille);
+    // printf("\n test ... ");
+    // printf("\n %d",tab[0]);
+    // affiche_tab(tab,taille);
+    // trie_par_selection(tab,taille);
+    // affiche_tab(tab,taille);
+    
+    double M[2][10];
+    remplir_matrice_temp(M);
+    affiche_matrice(M);
 }
