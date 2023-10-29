a7eae968e6b5dedc670af53baec13ad6f814361b
souhail <souhalshiv@gmail.com>
2023-10-29 19:42:46 +0100

    add the sort algorithms implementations

---
+++ methodTri.h
@@ -0,0 +1,11 @@
+void bubbleSort(int arr[], int size);
+void selectionSort(int arr[], int size);
+void insertionSort(int arr[], int size);
+void mergeSort(int arr[], int left, int right);
+void quickSort(int arr[], int low, int high);
+void heapSort(int arr[], int size);
+void countingSort(int arr[], int size);
+void radixSort(int arr[], int size);
+void shellSort(int arr[], int size);
+void cocktailSort(int arr[], int size);
+
