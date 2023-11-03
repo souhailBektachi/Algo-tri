d678de6f056a97345b91f9accb20d8dfe7de6f5a
souhail <souhalshiv@gmail.com>
2023-11-03 23:31:02 +0100

    test

--- lib/sortLib.c
+++ lib/sortLib.c
@@ -1,10 +1,15 @@
-#include "methodTri.h"
+#include "sortLib.h"
+#include <stdlib.h>
 
-void bubbleSort(int arr[], int size) {
+void bubbleSort(int arr[], int size)
+{
     int temp;
-    for (int i = 0; i < size - 1; i++) {
-        for (int j = 0; j < size - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
+    for (int i = 0; i < size - 1; i++)
+    {
+        for (int j = 0; j < size - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
                 temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
@@ -12,33 +17,42 @@ void bubbleSort(int arr[], int size) {
         }
     }
 }
-void selectionSort(int arr[], int size) {
-    for (int i = 0; i < size - 1; i++) {
+void selectionSort(int arr[], int size)
+{
+    for (int i = 0; i < size - 1; i++)
+    {
         int minIndex = i;
-        for (int j = i + 1; j < size; j++) {
-            if (arr[j] < arr[minIndex]) {
+        for (int j = i + 1; j < size; j++)
+        {
+            if (arr[j] < arr[minIndex])
+            {
                 minIndex = j;
             }
         }
-        if (minIndex != i) {
+        if (minIndex != i)
+        {
             int temp = arr[i];
             arr[i] = arr[minIndex];
             arr[minIndex] = temp;
         }
     }
 }
-void insertionSort(int arr[], int size) {
-    for (int i = 1; i < size; i++) {
+void insertionSort(int arr[], int size)
+{
+    for (int i = 1; i < size; i++)
+    {
         int key = arr[i];
         int j = i - 1;
-        while (j >= 0 && arr[j] > key) {
+        while (j >= 0 && arr[j] > key)
+        {
             arr[j + 1] = arr[j];
             j = j - 1;
         }
         arr[j + 1] = key;
     }
 }
-static void merge(int arr[], int left, int mid, int right) {
+static void merge(int arr[], int left, int mid, int right)
+{
     int i, j, k;
     int n1 = mid - left + 1;
     int n2 = right - mid;
@@ -53,32 +67,40 @@ static void merge(int arr[], int left, int mid, int right) {
     i = 0;
     j = 0;
     k = left;
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
             arr[k] = L[i];
             i++;
-        } else {
+        }
+        else
+        {
             arr[k] = R[j];
             j++;
         }
         k++;
     }
 
-    while (i < n1) {
+    while (i < n1)
+    {
         arr[k] = L[i];
         i++;
         k++;
     }
 
-    while (j < n2) {
+    while (j < n2)
+    {
         arr[k] = R[j];
         j++;
         k++;
     }
 }
 
-void mergeSort(int arr[], int left, int right) {
-    if (left < right) {
+void mergeSort(int arr[], int left, int right)
+{
+    if (left < right)
+    {
         int mid = left + (right - left) / 2;
         mergeSort(arr, left, mid);
         mergeSort(arr, mid + 1, right);
@@ -86,12 +108,15 @@ void mergeSort(int arr[], int left, int right) {
     }
 }
 
-static int partition(int arr[], int low, int high) {
+static int partition(int arr[], int low, int high)
+{
     int pivot = arr[high];
     int i = (low - 1);
 
-    for (int j = low; j <= high - 1; j++) {
-        if (arr[j] < pivot) {
+    for (int j = low; j <= high - 1; j++)
+    {
+        if (arr[j] < pivot)
+        {
             i++;
             int temp = arr[i];
             arr[i] = arr[j];
@@ -106,8 +131,10 @@ static int partition(int arr[], int low, int high) {
     return (i + 1);
 }
 
-void quickSort(int arr[], int low, int high) {
-    if (low < high) {
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
         int pi = partition(arr, low, high);
 
         quickSort(arr, low, pi - 1);
@@ -115,20 +142,24 @@ void quickSort(int arr[], int low, int high) {
     }
 }
 
-static void heapify(int arr[], int size, int i) {
+static void heapify(int arr[], int size, int i)
+{
     int largest = i;
     int left = 2 * i + 1;
     int right = 2 * i + 2;
 
-    if (left < size && arr[left] > arr[largest]) {
+    if (left < size && arr[left] > arr[largest])
+    {
         largest = left;
     }
 
-    if (right < size && arr[right] > arr[largest]) {
+    if (right < size && arr[right] > arr[largest])
+    {
         largest = right;
     }
 
-    if (largest != i) {
+    if (largest != i)
+    {
         int temp = arr[i];
         arr[i] = arr[largest];
         arr[largest] = temp;
@@ -136,114 +167,143 @@ static void heapify(int arr[], int size, int i) {
     }
 }
 
-void heapSort(int arr[], int size) {
-    for (int i = size / 2 - 1; i >= 0; i--) {
+void heapSort(int arr[], int size)
+{
+    for (int i = size / 2 - 1; i >= 0; i--)
+    {
         heapify(arr, size, i);
     }
 
-    for (int i = size - 1; i > 0; i--) {
+    for (int i = size - 1; i > 0; i--)
+    {
         int temp = arr[0];
         arr[0] = arr[i];
         arr[i] = temp;
         heapify(arr, i, 0);
     }
 }
-void countingSort(int arr[], int size) {
+void countingSort(int arr[], int size)
+{
     int max = arr[0];
-    for (int i = 1; i < size; i++) {
-        if (arr[i] > max) {
+    for (int i = 1; i < size; i++)
+    {
+        if (arr[i] > max)
+        {
             max = arr[i];
         }
     }
 
-    int* count = (int*)malloc((max + 1) * sizeof(int));
-    int* output = (int*)malloc(size * sizeof(int));
+    int *count = (int *)malloc((max + 1) * sizeof(int));
+    int *output = (int *)malloc(size * sizeof(int));
 
-    for (int i = 0; i <= max; i++) {
+    for (int i = 0; i <= max; i++)
+    {
         count[i] = 0;
     }
 
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++)
+    {
         count[arr[i]]++;
     }
 
-    for (int i = 1; i <= max; i++) {
+    for (int i = 1; i <= max; i++)
+    {
         count[i] += count[i - 1];
     }
 
-    for (int i = size - 1; i >= 0; i--) {
+    for (int i = size - 1; i >= 0; i--)
+    {
         output[count[arr[i]] - 1] = arr[i];
         count[arr[i]]--;
     }
 
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++)
+    {
         arr[i] = output[i];
     }
 
     free(count);
     free(output);
 }
-static int getMax(int arr[], int size) {
+static int getMax(int arr[], int size)
+{
     int max = arr[0];
-    for (int i = 1; i < size; i++) {
-        if (arr[i] > max) {
+    for (int i = 1; i < size; i++)
+    {
+        if (arr[i] > max)
+        {
             max = arr[i];
         }
     }
     return max;
 }
 
-static void countSort(int arr[], int size, int exp) {
+static void countSort(int arr[], int size, int exp)
+{
     int output[size];
     int count[10] = {0};
 
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++)
+    {
         count[(arr[i] / exp) % 10]++;
     }
 
-    for (int i = 1; i < 10; i++) {
+    for (int i = 1; i < 10; i++)
+    {
         count[i] += count[i - 1];
     }
 
-    for (int i = size - 1; i >= 0; i--) {
+    for (int i = size - 1; i >= 0; i--)
+    {
         output[count[(arr[i] / exp) % 10] - 1] = arr[i];
         count[(arr[i] / exp) % 10]--;
     }
 
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++)
+    {
         arr[i] = output[i];
     }
 }
 
-void radixSort(int arr[], int size) {
+void radixSort(int arr[], int size)
+{
     int max = getMax(arr, size);
 
-    for (int exp = 1; max / exp > 0; exp *= 10) {
+    for (int exp = 1; max / exp > 0; exp *= 10)
+    {
         countSort(arr, size, exp);
     }
 }
-void shellSort(int arr[], int size) {
-    for (int gap = size / 2; gap > 0; gap /= 2) {
-        for (int i = gap; i < size; i++) {
+void shellSort(int arr[], int size)
+{
+    for (int gap = size / 2; gap > 0; gap /= 2)
+    {
+        for (int i = gap; i < size; i++)
+        {
             int temp = arr[i];
             int j;
-            for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
+            for (j = i; j >= gap && arr[j - gap] > temp; j -= gap)
+            {
                 arr[j] = arr[j - gap];
             }
             arr[j] = temp;
         }
     }
 }
-void cocktailSort(int arr[], int size) {
+void cocktailSort(int arr[], int size)
+{
     int swapped = 1;
     int start = 0;
     int end = size - 1;
 
-    while (swapped) {
+    while (swapped)
+    {
         swapped = 0;
 
-        for (int i = start; i < end; ++i) {
-            if (arr[i] > arr[i + 1]) {
+        for (int i = start; i < end; ++i)
+        {
+            if (arr[i] > arr[i + 1])
+            {
                 // Swap arr[i] and arr[i + 1]
                 int temp = arr[i];
                 arr[i] = arr[i + 1];
@@ -252,15 +312,18 @@ void cocktailSort(int arr[], int size) {
             }
         }
 
-        if (!swapped) {
+        if (!swapped)
+        {
             break;
         }
 
         swapped = 0;
         --end;
 
-        for (int i = end - 1; i >= start; --i) {
-            if (arr[i] > arr[i + 1]) {
+        for (int i = end - 1; i >= start; --i)
+        {
+            if (arr[i] > arr[i + 1])
+            {
                 // Swap arr[i] and arr[i + 1]
                 int temp = arr[i];
                 arr[i] = arr[i + 1];
