5f3cd9c8b101678f6f444a1c8f65156188ef7e6a
Youssef Amazzal <20youssefyoussef02@gmail.com>
2023-11-03 23:08:10 +0100

    multithreading

--- lib/funcLib.c
+++ lib/funcLib.c
@@ -2,6 +2,7 @@
 #include "sortLib.h"
 #include <pthread.h>
 #include <string.h>
+#include <unistd.h>
 
 int INIT_SIZE, STEP, NB_STEP;
 
@@ -19,11 +20,11 @@ void random_v(int *tab, int size)
 // Fonction qui mesure le temps d'exécution d'une autre fonction
 double mesureTemps(void (*fonction)(), int size, int tab[])
 {
-    // int *tab;
-    // tab=(int*)malloc(size*sizeof(int));
 
     int *tabClone;
     tabClone = (int *)malloc(size * sizeof(int));
+
+    memccpy(tabClone, tab, size, sizeof(int));
 
     clock_t start, end;
     double cpu_time_used;
@@ -42,29 +43,55 @@ double mesureTemps(void (*fonction)(), int size, int tab[])
     return cpu_time_used;
 }
 // ----------------------------------------------------------------------------------------
+
+void *threadFunc(void *arg)
+{
+    ThreadData *data = (ThreadData *)arg;
+    *(data->recordTab) = (point){data->size, mesureTemps(data->sort, data->size, data->tab)};
+    data->done = 1;
+    printf("%s, ", data->name);
+    return NULL;
+}
+
 void remplir_matrice_temp(point M[][NB_STEP])
 {
     int i = 0, *tab, size = INIT_SIZE;
+    pthread_t threads[sizeof(ALGO_LIST) / sizeof(sortAlgo)][NB_STEP];
+    ThreadData threadData[sizeof(ALGO_LIST) / sizeof(sortAlgo)][NB_STEP];
+    int *pointers[NB_STEP];
+
     do
     {
         tab = (int *)malloc(size * sizeof(int));
-        if (tab == NULL)
-        {
-            printf("Erreur d'allocation de mémoire\n");
-            exit(1);
-        }
+        pointers[i] = tab;
 
         random_v(tab, size);
 
         for (int j = 0; j < sizeof(ALGO_LIST) / sizeof(sortAlgo); j++)
         {
-            M[j][i] = (point){size, mesureTemps(ALGO_LIST[j].sort, size, tab)};
+            threadData[j][i] = (ThreadData){&M[j][i], ALGO_LIST[j].sort, size, tab, ALGO_LIST[j].name};
+            pthread_create(&threads[j][i], NULL, threadFunc, &threadData[j][i]);
         }
 
         size += STEP;
-        free(tab);
         i++;
     } while (i < NB_STEP);
+
+    showLoading(threadData);
+
+    for (int i = 0; i < sizeof(ALGO_LIST) / sizeof(sortAlgo); i++)
+    {
+        for (int j = 0; j < NB_STEP; j++)
+        {
+            pthread_join(threads[i][j], NULL);
+        }
+    }
+
+    for (int i = 0; i < NB_STEP; i++)
+    {
+        free(pointers[i]);
+    }
+    printf("\n");
 }
 // ----------------------------------------------------------------------------------------
 void affiche_matrice(point M[][NB_STEP])
@@ -99,4 +126,39 @@ void affiche_matrice(point M[][NB_STEP])
         printf("|");
     }
     printf("\n");
+}
+
+void showLoading(ThreadData data[][NB_STEP])
+{
+    int algoCount = sizeof(ALGO_LIST) / sizeof(sortAlgo);
+    int allDone = 0;
+    int counter = 0;
+
+    while (!allDone)
+    {
+        printf("\033[H\033[J");
+        printf("counter: %d\n", counter++);
+
+        allDone = 1;
+
+        for (int i = 0; i < algoCount; i++)
+        {
+            int algoDone = 1;
+
+            for (int j = 0; j < NB_STEP; j++)
+            {
+                if (!data[i][j].done)
+                {
+                    algoDone = 0;
+                    allDone = 0;
+                }
+            }
+
+            printf("%-40s %s\n", data[i][0].name, algoDone ? "Done" : "Waiting...");
+        }
+
+        sleep(1);
+    }
+
+    printf("\033[H\033[J");
 }
--- lib/funcLib.h
+++ lib/funcLib.h
@@ -9,9 +9,21 @@ typedef struct point
     double time;
 } point;
 
+typedef struct
+{
+    point *recordTab;
+    void (*sort)();
+    int size;
+    int *tab;
+    char *name;
+    int done;
+} ThreadData;
+
 void random_v(int *tab, int taille);
 double mesureTemps(void (*fonction)(), int size, int *tab);
 void remplir_matrice_temp(point M[2][10]);
 void affiche_matrice(point M[2][10]);
 
 extern int INIT_SIZE, STEP, NB_STEP;
+
+void showLoading(ThreadData data[][NB_STEP]);
