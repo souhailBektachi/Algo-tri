57119cc7be82c120ef83ea4629ea00f1f5f08068
souhail <souhalshiv@gmail.com>
2023-11-05 14:15:30 +0100

    add the header for the cases

---
+++ lib/cas.h
@@ -0,0 +1,8 @@
+int* generateSortedArray(int size);
+int* generateBalancedPartitionsArray(int size);
+int* generateLimitedRangeArray(int size, int range);
+int* generateSmallSubarraySizesArray(int size);
+int* generateReverseSortedArray(int size);
+int* generateUnbalancedPartitionsArray(int size);
+int* generateLargeRangeArray(int size, int range);
+int* generateLargeInputArray(int size);
